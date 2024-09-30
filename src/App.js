2fc2fdd92193079d1aa77ebc3fcb2f8441aa0758
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-09-30 17:52:05 +0530

    created UI of app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
   return (
     <div className="App">
-      <h1>Jai Shree Ram</h1>
+      <div className="container">
+        <form>
+          <h1>BMI Calculator</h1>
+          <div>
+            <label>Weight(in Kgs)</label>
+            <input placeholder="Enter Weight value" value={weight} onChange={e => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height(in metres)</label>
+            <input placeholder="Enter Height value" value={height} onChange={e => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button>Submit</button>
+            <button>Reload</button>
+          </div>
+          <div>
+            <h3>Your BMI is : </h3>
+            <p>message come according to your BMI</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
