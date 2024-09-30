3257c5eb9fd3d11f794475148ab59b0576c8f857
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-09-30 18:47:06 +0530

    complete and some css changes

--- src/App.js
+++ src/App.js
@@ -6,26 +6,54 @@ function App() {
   const [height, setHeight] = useState('');
   const [bmi, setBmi] = useState('');
   const [message, setMessage] = useState('');
+
+  function reload() {
+    window.location.reload(true);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      alert('Please enter the valid values');
+    }
+    else {
+      let bmi = (weight / (height * height)).toFixed(1);
+      console.log(bmi);
+      setBmi(bmi);
+
+      if (bmi > 25)
+        setMessage('you are OverWeight');
+      else if (bmi > 18 && bmi <= 25)
+        setMessage('You are well maintained');
+      else if (bmi <= 18) {
+        setMessage('You are underWeight');
+
+      }
+
+    }
+
+
+  }
   return (
     <div className="App">
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>BMI Calculator</h1>
           <div>
             <label>Weight(in Kgs)</label>
-            <input placeholder="Enter Weight value" value={weight} onChange={e => setWeight(e.target.value)} />
+            <input placeholder="Enter Weight value" value={weight} onChange={e => setWeight(e.target.value)} type="number" />
           </div>
           <div>
             <label>Height(in metres)</label>
-            <input placeholder="Enter Height value" value={height} onChange={e => setHeight(e.target.value)} />
+            <input placeholder="Enter Height value" value={height} onChange={e => setHeight(e.target.value)} type="number" />
           </div>
           <div>
             <button>Submit</button>
-            <button>Reload</button>
+            <button onClick={reload}>Reload</button>
           </div>
           <div>
-            <h3>Your BMI is : </h3>
-            <p>message come according to your BMI</p>
+            <h3>Your BMI is :{bmi} </h3>
+            <p>{message}</p>
           </div>
         </form>
       </div>
